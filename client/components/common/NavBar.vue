e7a66c40ecc7dea2f3097d28a51b6a3a26c3dc83
Peiqi Chen <peiqichen0102@gmail.com>
2022-12-09 16:43:06 -0500

    Adding first draft of logo

--- client/components/common/NavBar.vue
+++ client/components/common/NavBar.vue
@@ -5,7 +5,7 @@
 <template>
   <v-app-bar color="accent" elevation="1">
     <div class="left">
-      <img src="../../public/logo.svg">
+      <img src="../../public/logo2.png">
       <h1 class="title">
         InterClass
       </h1>

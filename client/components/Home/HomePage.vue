74d5efdb4ff70efcb289b91c0bc580bbc113d2ee
Peiqi Chen <peiqichen0102@gmail.com>
2022-12-11 15:53:32 -0500

    Adding FAQ to home page

--- client/components/Home/HomePage.vue
+++ client/components/Home/HomePage.vue
@@ -1,9 +1,101 @@
 <template>
     <main>
-        <section>
-            <h2>Welcome to InterClass</h2>
+        <v-card-title primary-title class="justify-center">
+        <div>
+            <div class="text-h1">Welcome to InterClass</div>
+            <p class="text-center">Use InterClass to view classes, competitions, assignments, and more!</p>
+        </div>
+        </v-card-title>
+        <v-expansion-panels>
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                What is InterClass?
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                Growing up, we all know how hard it is to do homework. Every kid has something they would rather do instead of schoolwork. 
+                <br/> <br/> That's where InterClass comes in. InterClass helps make learning fun and easy, allowing classes to complete assignments, earn points and rewards, and compete against other classes. InterClass keeps students engaged in learning and motivated to complete assignments. InterClass is best aimed towards elementary school classes for when teachers want to introduce concepts like basic math. 
+                <br/> <br/> If this all sounds like everything you've always wanted, come join us in the magic of InterClass!
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                What can I use the Class page for?
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                When you start out on InterClass, the first thing to do is to look at the Class page. 
+                <br/> <br/> The Class page shows the amount of points your class has earned, and all of the students in your class. 
+                As a teacher, you have the power to create a class, add students into your class, and remove students from your class. 
+                <br/> <br/> As a student, you can be added to a class by your teacher.
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                What can I use the Competition page for?
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                Competitions are a way for classes to compete against each other. Students can work through the same assignments and earn points for their class. 
+                <br/> <br/> As a teacher, once you have a class, you can either create a new competition or join an ongoing competition on the Competition page. 
+                You can create or delete assignments for that competition. Afterwards, you can leave or end the competition that you are currently in. 
+                <br/> <br/> As a student, you can work on and solve assignments in the competition that your class is participating in.
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                What is an assignment? 
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                An assignment is made of one or more problems. Each assignment can be created by a teacher, and solved by students, in a competition.
+                <br/> <br/> A problem has a question, four answer choices, a correct answer, and a point value. When a student solves a problem, they earn the points for that problem for their class. 
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                I'm a teacher in a competition. How do I make an assignment? 
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                When you join a competition, you have the option to create a new assignment. 
+                <br/> <br/> First, give your assignment a name, like "Fractions Practice". 
+                Once you've made the assignment, you can add new problems to the assignment. For each problem, you can enter in the question, the point value, and the four answer choices. You can mark the correct answer by checking the checkbox next to an answer choice. 
+                <br/> <br/> If there are any errors with your input, you'll be able to see a message that shows what's wrong so you can correct it.
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                I'm a student in a competition. How do I know if I already completed an assignment?
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                Good question! When you go to the Competition page and click on an assignment, you will be able to view all of the problems in the assignment. Each problem will show either "Solved" or "Not Solved" for you.
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                What are hints? How do I use them?
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                Hints are rewarded to students in a class once the class's total points reaches a milestone goal during a competition. A student can use hints on an assignment to eliminate incorrect answer choices. The amount of hints is limited, so be sure to use them carefully! 
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+            <v-expansion-panel-header>
+                How can I see how well my class is doing in a competition?
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                You can use the Leaderboard page to view the current points of the classes participating in the competition. Alternatively, you can also view your own class's points on the Class page.
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+
+        </v-expansion-panels>
+        <!-- <section>
+            <div class="text-h1">Welcome to InterClass</div>
         </section>
-        <p>Use InterClass to view classes, competitions, assignments, and more!</p>
+        <p class="text-center">Use InterClass to view classes, competitions, assignments, and more!</p> -->
     </main>
 </template>
 
